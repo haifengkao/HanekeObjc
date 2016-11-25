5ad98e6815b15b0197748dcc80da8bcc39d33744
Hai Feng Kao <haifeng@cocoaspice.in>
2016-11-25 10:44:25 +0800

    update pods

--- Example/Pods/Target Support Files/Pods-HanekeObjc_Tests/Pods-HanekeObjc_Tests-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-HanekeObjc_Tests/Pods-HanekeObjc_Tests-frameworks.sh
@@ -86,12 +86,8 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/AltHanekeSwift/AltHaneke.framework"
   install_framework "$BUILT_PRODUCTS_DIR/HanekeObjc/HanekeObjc.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Nimble/Nimble.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Quick/Quick.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/AltHanekeSwift/AltHaneke.framework"
   install_framework "$BUILT_PRODUCTS_DIR/HanekeObjc/HanekeObjc.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Nimble/Nimble.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Quick/Quick.framework"
 fi
